b2c1283d4d8b12bdb39d904a450d0f3d0b178b32
byongyul <byongyul@gmail.com>
2020-03-15 23:18:18 +0900

    align tag with prettier tool

--- client/src/App.js
+++ client/src/App.js
@@ -1,71 +1,83 @@
-import React, {Component} from 'react';
-import Customer from './components/Customer';
-import './App.css';
-import Paper from '@material-ui/core/Paper';
-import Table from '@material-ui/core/Table';
-import TableHead from '@material-ui/core/TableHead';
-import TableBody from '@material-ui/core/TableBody';
-import TableRow from '@material-ui/core/TableRow';
-import TableCell from '@material-ui/core/TableCell';
-import {withStyles}from '@material-ui/core/styles';
+import React, { Component } from "react";
+import Customer from "./components/Customer";
+import "./App.css";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableBody from "@material-ui/core/TableBody";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import { withStyles } from "@material-ui/core/styles";
 
-const styles= theme=>({
-  root:{
-    width:'100%',
-    marginTop: theme.spacing.unit*3,
-    overflowX:"auto"
+const styles = theme => ({
+  root: {
+    width: "100%",
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
   },
-  table:{
-    minWidth:1080
+  table: {
+    minWidth: 1080
   }
-})
-const customers = [{
-  'id':1,
-  'name':'최예지',
-  'image':'https://www.placeimg.com/64/64/1',
-  'birthday':'030717',
-  'gender': 'female',
-  'job':'high school student'
-},
-{
-  'id':2,
-  'name':'최윤서',
-  'image':'https://www.placeimg.com/64/64/2',
-  'birthday':'011218',
-  'gender': 'male',
-  'job':'high school student'
-},
-{
-  'id':3,
-  'name':'홍길동',
-  'image':'https://www.placeimg.com/64/64/10',
-  'birthday':'960717',
-  'gender': 'female',
-  'job':'high school student'
-},
-]
-class App extends Component{
+});
+const customers = [
+  {
+    id: 1,
+    name: "최예지",
+    image: "https://www.placeimg.com/64/64/1",
+    birthday: "030717",
+    gender: "female",
+    job: "high school student"
+  },
+  {
+    id: 2,
+    name: "최윤서",
+    image: "https://www.placeimg.com/64/64/2",
+    birthday: "011218",
+    gender: "male",
+    job: "high school student"
+  },
+  {
+    id: 3,
+    name: "홍길동",
+    image: "https://www.placeimg.com/64/64/10",
+    birthday: "960717",
+    gender: "female",
+    job: "high school student"
+  }
+];
+class App extends Component {
   render() {
-    const {classes}=this.props;
+    const { classes } = this.props;
     return (
       <Paper className={classes.root}>
         <Table className={classes.table}>
           <TableHead>
-            <TableCell>ID</TableCell>
-            <TableCell>사진</TableCell>
-            <TableCell>이름</TableCell>
-            <TableCell>생년월일</TableCell>
-            <TableCell>성별</TableCell>
-            <TableCell>직업</TableCell>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell>사진</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
           </TableHead>
           <TableBody>
-      {customers.map(c=>{
-         return <Customer key={c.id}id={c.id}name={c.name}image={c.image}birthday={c.birthday}gender={c.gender}job={c.job}/> 
-      })}
+            {customers.map(c => {
+              return (
+                <Customer
+                  key={c.id}
+                  id={c.id}
+                  name={c.name}
+                  image={c.image}
+                  birthday={c.birthday}
+                  gender={c.gender}
+                  job={c.job}
+                />
+              );
+            })}
           </TableBody>
         </Table>
       </Paper>
-      
     );
   }
 }
--- server.js
+++ server.js
@@ -6,8 +6,39 @@ const port = process.env.PORT ||5000;
 app.use(bodyParser.json());
 app.unsubscribe(bodyParser.urlencoded({extended :true}));
 
-app.get('/api/hello',(req, res)=> {
-    res.send({message:'Hello Express!'});
-});
+//app.get('/api/hello',(req, res)=> {
+//    res.send({message:'Hello Express!'});
+//});
+app.get('/api/customers',(req,res)=>{
+    res.send([{
+        'id':1,
+        'name':'최예지',
+        'image':'https://www.placeimg.com/64/64/1',
+        'birthday':'030717',
+        'gender': 'female',
+        'job':'high school student'
+      },
+      {
+        'id':2,
+        'name':'최윤서',
+        'image':'https://www.placeimg.com/64/64/2',
+        'birthday':'011218',
+        'gender': 'male',
+        'job':'high school student'
+      },
+      {
+        'id':3,
+        'name':'홍길동',
+        'image':'https://www.placeimg.com/64/64/10',
+        'birthday':'960717',
+        'gender': 'female',
+        'job':'high school student'
+      },
+      ]
 
+
+    );
+
+
+})
 app.listen(port,()=>console.log(`Listening on port ${port}`));
